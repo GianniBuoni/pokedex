4e629420b895d0eab1936bf7ba4a64cac159bba8
GianniBuonni <162598175+GianniBuoni@users.noreply.github.com>
2024-05-07 00:06:39 -0400

    🧹🧹

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "My pokedex!",
+  title: "My Pokedex!",
   description: "Gianni tries to make his first framer motion project.",
 };
 
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gray-800`}>{children}</body>
     </html>
   );
 }
